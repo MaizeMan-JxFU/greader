035a04248298553e30b07ab85c5d53a5e60f5b83
Jingxian <827888724@qq.com>
2025-10-03 21:03:41 +0800

    Delete test files

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from greader import hmpreader
-from greader.base import genotype2vcf
-
-if __name__ == "__main__":
-    genotype = hmpreader(r'''C:\Users\82788\Desktop\Pyscript\greader\test\genotype.hmp''',4)
-    genotype2vcf(genotype,'test')
-    pass
