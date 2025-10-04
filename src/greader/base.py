f515c9b2a210b6bd01cbdca0520856d8306f4eaa
Jingxian <827888724@qq.com>
2025-10-04 11:51:22 +0800

    Fixed some bugs

--- src/greader/base.py
+++ src/greader/base.py
@@ -1,6 +1,5 @@
 from .ref_adjust import GENOMETOOL
 from bed_reader import open_bed
-import numpy as np
 import pandas as pd
 import gzip
 
@@ -9,7 +8,7 @@ import gzip
 def breader(prefix:str,ref_adjust:str=None) -> pd.DataFrame:
     '''ref_adjust: 基于基因组矫正, 需提供参考基因组路径'''
     with open_bed(f"{prefix}.bed",count_A1=False) as bed:
-        genotype = bed.read(dtype=np.int8)
+        genotype = bed.read(dtype='int8')
     fam = pd.read_csv(f'{prefix}.fam',sep=r'\s+',header=None)
     bim = pd.read_csv(f'{prefix}.bim',sep=r'\s+',header=None)
     genotype = pd.DataFrame(genotype,index=fam[0],).T
@@ -41,15 +40,15 @@ def vcfreader(vcfPath:str,chunksize=10_000,ref_adjust:str=None) -> pd.DataFrame:
                     col = line.replace('\n','').split('\t')
                     break
     ncol = [0,1,3,4]+list(range(col.index('FORMAT')+1,len(col)))
-    col = np.array(col)[ncol]
-    vcf_chunks = pd.read_csv(vcfPath,sep=r'\s+',comment='#',header=None,usecols=ncol,low_memory=True,compression=compression,chunksize=chunksize)
+    col = [col[i] for i in ncol]
+    vcf_chunks = pd.read_csv(vcfPath,sep=r'\s+',comment='#',header=None,usecols=ncol,low_memory=False,compression=compression,chunksize=chunksize)
     genotype = []
     for vcf_chunk in vcf_chunks: # 分块处理vcf
         vcf_chunk:pd.DataFrame = vcf_chunk.set_index([0,1]).fillna('-9')
         ref_alt = vcf_chunk.iloc[:,:2]
         def transG(col:pd.Series):
-            vcf_transdict = {'0/0':0,'1/1':2,'0/1':1,'1/0':1,'./.':-9,'0|0':0,'1|1':2,'0|1':1,'1|0':1,'.|.':-9}
-            return col.map(vcf_transdict).astype(np.int8)
+            vcf_transdict = {'0/0':0,'1/1':2,'0/1':1,'1/0':1,'./.':-9,'0|0':0,'1|1':2,'0|1':1,'1|0':1,'.|.':-9,'2':2,'1':1,'0':0,'.':-9}
+            return col.map(vcf_transdict).astype('int8')
         vcf_chunk = vcf_chunk.iloc[:,2:].apply(transG,axis=0)
         vcf_chunk = pd.concat([ref_alt,vcf_chunk],axis=1)
         genotype.append(vcf_chunk)
@@ -89,7 +88,7 @@ def hmpreader(hmp:str,sample_start:int=None,chr:str='chrom',ps:str='position',re
         ar = ref_alt['A1']+ref_alt['A0']
         aa = ref_alt['A1']+ref_alt['A1']
         def hmp2genotype(col:pd.Series):
-            return ((col==ra)|(col==ar)).astype(np.int8)+2*(col==aa).astype(np.int8)
+            return ((col==ra)|(col==ar)).astype('int8')+2*(col==aa).astype('int8')
         genotype = genotype.loc[ref_alt.index]
         xxmask = (genotype=='XX')
         genotype = genotype.apply(hmp2genotype,axis=0)
